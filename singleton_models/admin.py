592c8de4c4921cd5690244ec03387676398aae76
Thomas Randle <thomas@ionata.com.au>
2014-09-11 16:17:17 +1000

    1.7 dep

--- singleton_models/admin.py
+++ singleton_models/admin.py
@@ -39,7 +39,7 @@ class SingletonModelAdmin(admin.ModelAdmin):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return wrapper
 
-        info = self.model._meta.app_label, self.model._meta.module_name
+        info = self.model._meta.app_label, self.model._meta.model_name
 
         urlpatterns = patterns(
             '',
